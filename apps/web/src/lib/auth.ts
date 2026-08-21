345fdd564fbd0671a45e377dc33fc5ea5bc5120b
chaitanya-kolliboyina <kvschaitanya17@gmail.com>
2026-08-21 20:18:06 +0530

    errors

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -8,12 +8,12 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID || "dummy-github-id",
+      clientSecret: process.env.GITHUB_SECRET || "dummy-github-secret",
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "dummy-google-id",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "dummy-google-secret",
     }),
   ],
   callbacks: {
